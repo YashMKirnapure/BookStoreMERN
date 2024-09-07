7400e12d3fd1bd1e443ec32c05a2aaddd8c5ccae
YashKirnapure77 <yashkirnapure777@gmail.com>
2024-09-07 18:08:30 +0530

    Added Initial backend setup

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bookRoute from "./route/book.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+//connecting to MongoDB
+try{
+    mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("Connected to mongoDB");
+}catch(error)
+{
+    console.log("Error: ",error);
+}
+
+//defining routes
+app.use("/book",bookRoute)
+
+app.listen(PORT,()=>{
+    console.log(`Example app listening on port ${PORT}`);
+});
---
+++ Backend/route/book.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getBook } from '../controller/book.controller';
+
+const router = express.Router();
+
+
+router.get("/",getBook);
+
+export default router;
